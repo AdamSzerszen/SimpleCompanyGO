1f27a78663819cc6e905efdab9de0c82327f21ce
Adam Szerszeń <adam_szerszen@interia.pl>
2018-12-20 15:14:05 +0100

    Modified operationGenerator to use IOperator interface to be SOLID friendly

--- src/generators/operationGenerator.go
+++ src/generators/operationGenerator.go
@@ -1,27 +1,33 @@
 package generators
 
 import (
+	"../operators"
 	"math/rand"
 	"time"
 )
 
 type IOperationGenerator interface {
-	GetRandomOperation() string
+	GetRandomOperation() *operators.IOperator
+	AddOperation(operator *operators.IOperator)
 }
 
 type OperationGenerator struct {
-	availableOperations []string
+	availableOperations []*operators.IOperator
 }
 
-func (operationGenerator OperationGenerator) GetRandomOperation() string {
+func (operationGenerator OperationGenerator) GetRandomOperation() *operators.IOperator {
 	rand.Seed(time.Now().UnixNano())
 	operationsRange := len(operationGenerator.availableOperations)
 	randIndex := rand.Int() % operationsRange
 	return operationGenerator.availableOperations[randIndex]
 }
 
-func CreateOperationGenerator(availableOperations []string) *IOperationGenerator {
-	generator := new(OperationGenerator{availableOperations: availableOperations})
+func (operationGenerator OperationGenerator) AddOperation(operator *operators.IOperator) {
+	operationGenerator.availableOperations = append(operationGenerator.availableOperations, operator)
+}
+
+func CreateOperationGenerator() *IOperationGenerator {
+	generator := new(OperationGenerator{availableOperations: []*operators.IOperator{}})
 
 	return generator
 }
